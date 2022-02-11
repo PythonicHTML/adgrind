82f452ba2d0575402fb7d27f3f16c5ec33d91ca7
PythonicHTML <98992286+PythonicHTML@users.noreply.github.com>
2022-02-11 16:28:52 -0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,17 @@
+import time
+import mouse
+time.sleep(2)
+me = mouse.get_position()
+print(me)
+mouse.move(550, 330, absolute=True, duration=0)
+time.sleep(0.3)
+mouse.click('left')
+time.sleep(0.3)
+mouse.move(820, 625, absolute=True, duration=0)
+time.sleep(0.3)
+iterations = 20
+while iterations > 0:
+    mouse.click('left')
+    iterations -= 1
+    time.sleep(95)
+
